4c69aa8b47bb4307ec090b6a1fe25ba6eb76f1b2
Pratikshit26 <pratikshitpfd@gmail.com>
2021-06-26 23:24:05 +0530

    stauts and landing bay with tiles

---
+++ app/containers/HomePage/data.js
@@ -0,0 +1,26 @@
+export var data = [
+  {
+    id: "01",
+    image:
+      "https://a0.muscache.com/im/pictures/2f13349d-879d-43c6-83e3-8e5679291d53.jpg?im_w=720",
+    type: "Outdoor Gateway",
+  },
+  {
+    id: "02",
+    image:
+      "https://a0.muscache.com/im/pictures/36f53e61-db8d-403c-9122-5b761c0e4264.jpg?im_w=720",
+    type: "Unique Stays",
+  },
+  {
+    id: "03",
+    image:
+      "https://a0.muscache.com/im/pictures/7d82ca14-56e5-4465-8218-dcfa7d69b6ac.jpg?im_w=720",
+    type: "Entire Homes",
+  },
+  {
+    id: "04",
+    image:
+      "https://a0.muscache.com/im/pictures/10a638e1-6aff-4313-8033-1275cec83987.jpg?im_w=720",
+    type: "Pets Allowed",
+  },
+];
--- app/containers/HomePage/homepage2.js
+++ app/containers/HomePage/homepage2.js
@@ -11,7 +11,8 @@ import { FormattedMessage } from "react-intl";
 import { connect } from "react-redux";
 import { compose } from "redux";
 import { createStructuredSelector } from "reselect";
-
+import { data } from "./data.js";
+import { Card } from "antd";
 import { useInjectReducer } from "utils/injectReducer";
 import { useInjectSaga } from "utils/injectSaga";
 import {
@@ -23,11 +24,49 @@ import H2 from "components/H2";
 import NavBar from "components/Navbar";
 // import "../../styles/home2.css";
 
-const key = "home";
+const { Meta } = Card;
+
 function Homepage2() {
+  const [lon, setLon] = React.useState("");
+  const [lat, setLat] = React.useState("");
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLat(position.coords.latitude);
+      setLon(position.coords.longitude);
+      console.log(data);
+    });
+  }, []);
   return (
-    <div className="homepage2">
+    <div
+      className="homepage2"
+      style={{ marginTop: "150px", padding: "0px 70px" }}
+    >
       <H2>Explore nearby</H2>
+      <p>
+        Your current position is <strong>{lat} latitude</strong>{" "}
+        <strong>{lon} longitude</strong>
+      </p>
+      <H2>Live anywhere</H2>
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          borderradius: "12px",
+        }}
+      >
+        {data.map((d) => (
+          <Card
+            hoverable
+            style={{ width: 300, borderRadius: "10px" }}
+            cover={<img src={d.image} style={{ borderRadius: "10px" }} />}
+            key={d.id}
+          >
+            <Meta title={d.type} />
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
